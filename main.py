409a7de0077b0ac6c2b365d50ad6740325c15a32
Klementina91 <tatyana.klemenova@mail.ru>
2024-09-24 18:14:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# 1st program
+print(9 ** 0.5)
+print(3.0 * 5)
+# 2nd program
+print(9.99 < 9.98 or 1000 != 1000.1)
+# 3rd program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(6 == 8)
+# 4th program
+print("123.456")
+print(type(123.456))
+print(123.456 * 10)
+print(1234.56 % 10)
+print((int(4.0)))
