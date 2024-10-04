ab69e02dd3db46d6a09a8050268c64f3308e6e76
Klementina91 <tatyana.klemenova@mail.ru>
2024-10-04 18:55:42 +0300

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -2,7 +2,7 @@ name = "Tatiana"
 print(name) #string
 age = 44
 print(age) #int
-new_age = (age + 1)
-print(new_age) #сложение
+age = (age + 1)
+print(age) #сложение
 is_student = True
 print(is_student) #bool
