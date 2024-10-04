f3f700095e9dfaf3a07dd6985e429886bb14f6fa
Klementina91 <tatyana.klemenova@mail.ru>
2024-10-04 18:46:34 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+completed_homework_count=12
+hours_spent=1.5
+cours_name ='Python'
+time_per_task=hours_spent/completed_homework_count
+print('Курс:', cours_name,', всего задач:',completed_homework_count,', затрачено часов:',hours_spent,', среднее время выполнения ',time_per_task, ' часа','.',sep='')
