01bc7eb3beeceaa06d3ac630156f43fd62ea982f
Klementina91 <tatyana.klemenova@mail.ru>
2024-10-06 12:11:27 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+my_string=input('Какое время года:')
+print(len(my_string))
+print(my_string.upper()) #Вывожу строку в верхнем регистре
+print(my_string.lower()) #Вывожу строку в нижнем регистре
+my_string='Какое время года'
+my_string=my_string.replace(' ','')
+print(my_string)
+print(my_string[0]) #Вывожу первый символ строки
+print(my_string[-1]) #Вывожу последний символ строки
