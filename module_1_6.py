0fbe4497920b4231770a3073651e17a7537a0b9a
Klementina91 <tatyana.klemenova@mail.ru>
2024-10-08 17:08:56 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,16 @@
+my_dict={'Tatyana': 1980, 'Anna': 1955, 'Tanya': 2006}
+print(my_dict)
+print(my_dict['Anna'])
+print(my_dict.get('Egor'))
+my_dict.update({'Elena': 1983, 'Svetlana': 1977})
+print(my_dict.pop('Tatyana'))
+print(my_dict)
+
+my_set={False, False, True, False, '(mother, father, son)', '(mother, father, son)', '(mother, father, son)', 'set', 1, 2, 3, 3, 5, 2}
+print(my_set)
+my_set.add('Осень')
+my_set.add('3.25')
+my_set.discard(True)
+print(my_set)
+
+
